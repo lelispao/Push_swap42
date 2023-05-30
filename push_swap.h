28badba496f283bc449efe8e333fd308911542a2
Lefter Kioukou <lkioukou@c4a12c4.42berlin.de>
2023-05-30 16:47:44 +0200

    test

---
+++ push_swap.h
@@ -0,0 +1,14 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "ft_printf.h"
+
+size_t  ft_strlen(const char *s);
+int ft_printf(const char *str, ...);
+
+
+
+#endif 
---
+++ test.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkioukou <lkioukou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 14:49:45 by lkioukou          #+#    #+#             */
+/*   Updated: 2023/05/30 16:29:21 by lkioukou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ok merge sort
+1) len list    2) /2  until theres 2 for every /2     522,,,313 so /2 the len of every sub and its 3 and sort the way up recussion might work
+3)
+
+
+
+
+
+*/ 
